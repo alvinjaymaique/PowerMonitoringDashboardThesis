48202c1ae8d647b87bfb62c991b6e0c14476d2b7
alvinjaymaique <alvinjay.maique.56@gmail.com>
2025-02-08 12:43:20 +0800

    added list display 'power', 'frequency'

--- backend/power_monitor/admin.py
+++ backend/power_monitor/admin.py
@@ -5,5 +5,5 @@ from .models import ElectricalParameter
 
 @admin.register(ElectricalParameter)
 class ElectricalParameterAdmin(admin.ModelAdmin):
-    list_display = ('timestamp', 'voltage', 'current', 'power_factor', 'is_anomaly')
+    list_display = ('timestamp', 'voltage', 'current', 'power', 'power_factor', 'frequency', 'is_anomaly')
     list_filter = ('is_anomaly', 'timestamp')
