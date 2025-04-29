e133c75ca8dc24ce9bcc0843f3450d852533cd89
alvinjaymaique <alvinjay.maique.56@gmail.com>
2025-04-29 15:09:25 +0800

    removed comments only

--- frontend/src/components/PowerGraph.jsx
+++ frontend/src/components/PowerGraph.jsx
@@ -266,7 +266,6 @@ const PowerGraph = ({ readings, graphType, selectedNode }) => {
               <h4>Data Points</h4>
               <p>{chartData.length}</p>
             </div>
-            {/* Anomalies count removed */}
           </div>
         </div>
           
